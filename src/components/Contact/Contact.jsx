20df1900bec6704af0dcdabe3886f0f1a87baf01
hasirali <hasira804@gmail.com>
2023-09-24 21:16:08 +0530

    Done

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
+                                    Kareli GTB Nagar Prayagraj India
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 1234567890
                                 </div>
                             </div>
 
@@ -82,7 +82,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    info@acme.org
+                                    hasira804@gmail.com
                                 </div>
                             </div>
                         </div>
